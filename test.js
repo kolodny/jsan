63e84fa74192deeacec24cebd38c48edc2fa9b64
Moshe Kolodny <mkolodny@integralads.com>
2015-03-24 09:05:04 -0400

    typos and tests

--- test.js
+++ test.js
@@ -78,5 +78,26 @@ describe('jsan', function() {
     });
   });
 
+  it('can round trip a regular object', function() {
+    var obj1 = {a: {b: {c: {d: 1}}}};
+    var obj2 = jsan.parse(jsan.stringify(obj1));
+    assert.deepEqual(obj1, obj2);
+  });
+
+  it('can round trip a circular object', function() {
+    var obj1 = {};
+    obj1['self'] = obj1;
+    var obj2 = jsan.parse(jsan.stringify(obj1));
+    assert(obj2['self'] === obj2);
+  });
+
+  it('can round trip a self referencing objects', function() {
+    var obj1 = {};
+    var subObj = {};
+    obj1.a = subObj;
+    obj1.b = subObj;
+    var obj2 = jsan.parse(jsan.stringify(obj1, null, null, true));
+    assert(obj2.a === obj2.b);
+  });
 
 });
