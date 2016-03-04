d7e410fc2934f0b3b0efb447f41a8410528954b4
Moshe Kolodny <kolodny.github@gmail.com>
2016-03-04 12:04:15 -0500

    merged #5 closes #5

---
+++ test/regressions.js
@@ -0,0 +1,10 @@
+var assert = require('assert');
+var jsan = require('../');
+
+describe('newline before function curly brace', function() {
+  var fn = function foo(a,b, c)
+  {
+    return 123;
+  };
+  assert.equal(jsan.stringify(fn, null, null, true), '{"$jsan":"ffunction foo(a,b, c) { /* ... */ }"}');
+})
