8ba3a29489c6b3b8f033e5d1cfc00b7e029830a4
Zalmoxisus <zalmoxisus@gmail.com>
2018-12-01 16:58:33 +0200

    Revert "fix(test): assert on deserialized object with references which should be resolved"
    This reverts commit 2f76353dc05a91d971b2d28575ffd2c3ff4f7233.

--- test/regression-remotedev-serialize.js
+++ test/regression-remotedev-serialize.js
@@ -17,7 +17,7 @@ describe('remotedev-serialize', function() {
         fst: new record(),
         scnd: new record(),
     });
-    debugger;
+
     var serialized = jsan.stringify(obj, serialize(immutable, refs).replacer, null, true);
     var parsed = JSON.parse(serialized);
 
@@ -28,6 +28,6 @@ describe('remotedev-serialize', function() {
     assert.ok(scndProp.hasOwnProperty('$jsan') && (typeof scndProp.$jsan === 'string' || parsed.data.scnd.data.prop.$jsan instanceof String));
     
     var deserialized = jsan.parse(serialized, serialize(immutable, refs).reviver);
-    assert.ok(Array.isArray(deserialized.get('scnd').get('prop')), 'deserialized structure has unresolved references');
+    assert.ok(Array.isArray(obj.get('fst').get('prop')));
   });
 });
